90bbc9525d8db54d71599f4d8de3564aa2064761
Kristina Meshcheryakova <yasherkavanilla@gmail.com>
2017-09-13 02:42:39 +0300

    Спозиционировала карту

--- js/script.js
+++ js/script.js
@@ -1,31 +1,17 @@
-ymaps.ready(function () {
-    var myMap = new ymaps.Map('map', {
-            center: [55.751574, 37.573856],
-            zoom: 9
-        }, {
-            searchControlProvider: 'yandex#search'
-        }),
+var myMap;
 
-        // Создаём макет содержимого.
-        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-        ),
+// Дождёмся загрузки API и готовности DOM.
+ymaps.ready(init);
 
-        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-            hintContent: 'Собственный значок метки',
-            balloonContent: 'Это красивая метка'
-        }, {
-            // Опции.
-            // Необходимо указать данный тип макета.
-            iconLayout: 'default#image',
-            // Своё изображение иконки метки.
-            iconImageHref: '../img/pin.svg',
-            // Размеры метки.
-            iconImageSize: [30, 42],
-            // Смещение левого верхнего угла иконки относительно
-            // её "ножки" (точки привязки).
-            iconImageOffset: [-5, -38]
-        });
-    myMap.geoObjects
-        .add(myPlacemark);
-});
+function init () {
+    // Создание экземпляра карты и его привязка к контейнеру с
+    // заданным id ("map").
+    myMap = new ymaps.Map('map', {
+        // При инициализации карты обязательно нужно указать
+        // её центр и коэффициент масштабирования.
+        center: [59.93848058432434,30.327623446101025],
+        zoom: 16
+    }, {
+        searchControlProvider: 'yandex#search'
+    });
+}
